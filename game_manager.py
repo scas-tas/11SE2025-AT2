49b49bd666d0f9255dca351a6512c43efa4064e2
missgrainger <42230764+missgrainger@users.noreply.github.com>
2025-05-26 13:04:17 +1000

    Create game_manager.py
    including file header

---
+++ game_manager.py
@@ -0,0 +1,7 @@
+# ==============================================
+# Project Name:      OOP Retro Game – [Game Title]
+# File Name:         game_manager.py
+# Author:            [Full Name]
+# Description:       Manages game state, scoring, and overall logic
+# ==============================================
+
