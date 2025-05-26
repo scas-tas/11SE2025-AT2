5a7f389096262eb11b6fa8d059fd119be9388e32
missgrainger <42230764+missgrainger@users.noreply.github.com>
2025-05-26 13:01:57 +1000

    Create main.py
    Including file header

---
+++ main.py
@@ -0,0 +1,11 @@
+# ==============================================
+# Project Name:      OOP Retro Game – [Game Title]
+# File Name:         main.py
+# Author:            [Full Name]
+# Class:             Year 11 Software Engineering
+# Date Commenced:    DD/MM/YYYY
+# Date Submitted:    DD/MM/YYYY
+# GitHub Repo Link:  [URL if required]
+# Description:       Entry point for game logic and main loop
+# ==============================================
+
