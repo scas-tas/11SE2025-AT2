dc987029132f1497454521856f6b6c3794220c77
missgrainger <42230764+missgrainger@users.noreply.github.com>
2025-05-26 13:03:04 +1000

    Create player.py
    including file header

---
+++ player.py
@@ -0,0 +1,7 @@
+# ==============================================
+# Project Name:      OOP Retro Game – [Game Title]
+# File Name:         player.py
+# Author:            [Full Name]
+# Description:       Defines the Player class and player actions
+# ==============================================
+
