7d95658aafcb565528f36602fc2389d35d596cb8
missgrainger <42230764+missgrainger@users.noreply.github.com>
2025-05-26 13:03:42 +1000

    Create obstacle.py
    including file header

---
+++ obstacle.py
@@ -0,0 +1,7 @@
+# ==============================================
+# Project Name:      OOP Retro Game – [Game Title]
+# File Name:         obstacle.py
+# Author:            [Full Name]
+# Description:       Base class for obstacles (e.g. cars, logs)
+# ==============================================
+
